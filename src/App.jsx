2645f1c20153a0c6a066c24a46ed84df8373cf79
JulienOza <julien.ozarowski@gmail.com>
2023-10-12 11:22:22 +0200

    add nav buttons using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,52 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
   {
-      name: "bulbasaur",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-      name: "mew",
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
   },
 ];
 
+
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const previousClick = () => {
+    setPokemonIndex(pokemonIndex - 1)
+  }
+
+  const nextClick = () => {
+    setPokemonIndex(pokemonIndex + 1)
+  }
+
   return (
-      <PokemonCard pokemon={pokemonList[0]}/>
+    <>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      <button onClick={previousClick} disabled={pokemonIndex === 0}>Previous</button>
+      <button onClick={nextClick} disabled={pokemonIndex === pokemonList.length - 1}>Next</button>
+    </>
   );
 }
 
